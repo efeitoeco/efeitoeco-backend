b6c3bc9be3cf6618e38efb20cb4ca8b526799495
Gabriel Fernandes Ribeiro <contato.gfernandesribeiro@outlook.com>
2021-04-03 01:44:14 -0300

    Adicionando configurações de segurança para get de produtos

--- eCommerce/src/main/java/com/efeitoeco/eCommerce/security/BasicSecurityConfig.java
+++ eCommerce/src/main/java/com/efeitoeco/eCommerce/security/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package com.efeitoeco.eCommerce.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,9 +31,12 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers("/**").permitAll()
 		.antMatchers("/").permitAll()
 		.antMatchers("/usuarios/logar").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers(HttpMethod.GET ,"/produtos").permitAll()
+		.antMatchers(HttpMethod.GET ,"/categorias").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
